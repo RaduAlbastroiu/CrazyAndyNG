5981436f25f0622e1874fac65b108bf6dc035c77
Radu Albastroiu <albastroiuradug@gmail.com>
2020-09-20 02:59:51 +0300

    Add api routes

--- backend/api.js
+++ backend/api.js
@@ -3,5 +3,7 @@ const { Router } = require('express');
 const mainRouter = new Router();
 
 // register routes
+const authRoutes = require('./auth/router');
+mainRouter.use('/auth', authRoutes);
 
 module.exports = mainRouter;
---
+++ backend/auth/router.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+
+const authRouter = new Router();
+
+module.exports = authRouter;
