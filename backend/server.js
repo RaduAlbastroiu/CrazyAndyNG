429bd86cd858c639d5fec9201cfa384fc1eaefe0
Radu Albastroiu <albastroiuradug@gmail.com>
2020-09-20 02:24:52 +0300

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// setup environment variables
+require('dotenv').config();
+//const connectDB = require('./db');
+
+// load database
+//connectDB();
+
+// spin up server
+//const app = require('./backend/app');
+//app.listen(process.env.PORT, () => {
+//  console.log(`listening on ${process.env.PORT}`);
+//});
