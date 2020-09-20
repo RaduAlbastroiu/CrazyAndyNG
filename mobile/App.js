eb0c78131f7410f5b33f7711ef728ff8a1c762f0
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-09-20 10:40:39 +0300

    initial commit
    initial commit

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App= () => {
+  return (
+    <View><Text>App screen</Text></View>
+  )
+}
+export default App;
