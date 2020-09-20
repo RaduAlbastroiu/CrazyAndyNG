d913cc56d66e201578976e9b301f91a1297ebc05
Radu Albastroiu <albastroiuradug@gmail.com>
2020-09-20 13:21:36 +0300

    Login route completed

---
+++ backend/auth/controller.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+class UserController {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async sign(user) {
+    return jwt.sign(
+      {
+        _id: user._id,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.SECRET,
+      { expiresIn: '24h' }
+    );
+  }
+
+  async login(user) {
+    const { email, password } = user;
+    const dbUser = await this.model.findOne({ email });
+
+    if (dbUser) {
+      const same = await this.model.comparePassword(password, dbUser.password);
+      if (same) {
+        return this.sign(dbUser);
+      }
+      throw 'password mismatch';
+    }
+    throw 'not found';
+  }
+}
+
+module.exports = UserController;
--- backend/auth/router.js
+++ backend/auth/router.js
@@ -3,19 +3,32 @@ const { check } = require('express-validator');
 
 const validate = require('../middleware/paramsValidation');
 
-const userRouter = new Router();
+const UserController = require('./controller');
+const UserModel = require('./model');
 
-userRouter.post('/login', [], validate, async (req, res) => {
-  try {
-  } catch (err) {
-    if (err === 'password mismatch' || err === 'not found') {
-      return res
-        .status(403)
-        .json({ err: 'Invalid email and password combination' });
+const userRouter = new Router();
+const userController = new UserController(UserModel);
+
+userRouter.post(
+  '/login',
+  [
+    check('email', 'Empty email field').not().isEmpty(),
+    check('email', 'Please provide valid email').isEmail(),
+    check('password', 'Empty password field').not().isEmpty(),
+  ],
+  validate,
+  async (req, res) => {
+    try {
+    } catch (err) {
+      if (err === 'password mismatch' || err === 'not found') {
+        return res
+          .status(403)
+          .json({ err: 'Invalid email and password combination' });
+      }
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
     }
-    console.error(err);
-    return res.status(500).send('Internal Server Error');
   }
-});
+);
 
 module.exports = userRouter;
