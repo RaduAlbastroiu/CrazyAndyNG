b619f645dd7dddc572237ce9f03aaeb283b460b2
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-09-24 00:10:50 +0300

    action button style changes

--- mobile/App.js
+++ mobile/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Navigator from './src/routes/drawer.js';
 import SplashScreen from 'react-native-splash-screen';
 import {useEffect} from 'react'
+import { LogBox } from 'react-native';
 
+LogBox.ignoreWarnings([
+  'Animated: `useNativeDriver` was not specified.',
+]);
 export default function App() {
   useEffect(() => {
     SplashScreen.hide();
--- mobile/src/shared/FloatingButton.js
+++ mobile/src/shared/FloatingButton.js
@@ -2,37 +2,56 @@ import React, {Component} from 'react';
 import {View, Text, Image, StyleSheet} from 'react-native';
 import ActionButton from 'react-native-action-button';
 import burger from '../assets/burger.png';
-
+<Image source={burger} />;
 const FloatingButton = () => {
- 
-    
-      return (
-        <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
-          {/* Rest of the app comes ABOVE the action button component !*/}
-          <ActionButton buttonColor="rgba(231,76,60,1)"onPress={()=>console.log('Mergi FMM')}>
-            <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
-            <Image source={burger} style={styles.actionButtonIcon} />
-            </ActionButton.Item>
-            <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-            <Image source={burger} style={styles.actionButtonIcon} />
-            </ActionButton.Item>
-            <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-            <Image source={burger} style={styles.actionButtonIcon} />
-            </ActionButton.Item>
-          </ActionButton>
-        </View>
-      );
-    
-   
-  }
-   
-  const styles = StyleSheet.create({
-    actionButtonIcon: {
-flex:1,      fontSize: 20,
-      height: 22,
-      color: 'white',
-    },
-  });
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>dddddddddddddddddddddddddddddddddddd dd</Text>
+      <ActionButton buttonColor="#329ba8">
+        <ActionButton.Item
+          hideLabelShadow={false}
+          buttonColor="#329ba8"
+          title="Terms and Conditions"
+          onPress={() => console.log('notes tapped!')}>
+          <></>
+        </ActionButton.Item>
+        <ActionButton.Item
+          hideLabelShadow={false}
+          buttonColor="#329ba8"
+          title="Privacy Policy"
+          onPress={() => console.log('notes tapped!')}>
+          <></>
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor="#329ba8" title="FAQ" onPress={() => {}}>
+          <></>
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#329ba8"
+          title="About Us"
+          onPress={() => {}}>
+          <></>
+        </ActionButton.Item>
+      </ActionButton>
+    </View>
+  );
+};
 
-  export default FloatingButton;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+  },
+});
 
+export default FloatingButton;
