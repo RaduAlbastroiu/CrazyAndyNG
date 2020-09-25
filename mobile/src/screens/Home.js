09b009063692377b520a3b48dfa549216310233f
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-09-25 18:59:24 +0300

    small translation update

--- mobile/src/screens/Home.js
+++ mobile/src/screens/Home.js
@@ -13,14 +13,13 @@ import i18n from 'i18next';
 import {useTranslation, initReactI18next} from 'react-i18next';
 
 const deviceLanguage =
-  Platform.OS === 'Android'
+  Platform.OS === 'Android || iOS'
     ? NativeModules.SettingsManager.settings.AppleLocale ||
       NativeModules.SettingsManager.settings.AppleLanguages[0] // iOS 13
     : NativeModules.I18nManager.localeIdentifier;
 
 console.log(deviceLanguage);
 
-// i18n.changeLanguage('en-US');
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
@@ -29,11 +28,13 @@ i18n
       en_US: {
         translation: {
           Home: 'Home',
+          "Start Tutorial":"Start Tutorial"
         },
       },
       zh: {
         translation: {
           Home: '家',
+          "Start Tutorial": '开始教程'
         },
       },
     },
@@ -49,16 +50,13 @@ i18n
 
 const Home = ({navigation}) => {
   const [showTutorial, setShowTutorial] = useState(true);
-  // const [languageSelected, setLanguageSelected] = useState('en');
   const {t} = useTranslation();
-  console.log(t);
 
   //give item
   const setData = async (value) => {
     try {
       setShowTutorial(false);
       await AsyncStorage.setItem('key', '27');
-      console.log('data set');
     } catch (e) {}
   };
 
@@ -93,7 +91,7 @@ const Home = ({navigation}) => {
       <TouchableOpacity
         onPress={onPressTutorial}
         style={styles.appButtonContainer}>
-        <Text style={styles.appButtonText}>Start Tutorial</Text>
+        <Text style={styles.appButtonText}>{t('Start Tutorial')}</Text>
       </TouchableOpacity>
     );
   };
