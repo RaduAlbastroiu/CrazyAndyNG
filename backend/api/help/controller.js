b01d24debd2f88adb1ff04c631b2b19a141f1b1f
Radu Albastroiu <albastroiuradug@gmail.com>
2020-09-26 19:47:19 +0300

    Add legal routes

--- backend/api/help/controller.js
+++ backend/api/help/controller.js
@@ -1,10 +1,34 @@
 const aboutHtml = require('../../resources/about');
+const privacyHtml = require('../../resources/privacyPolicy');
+const faqHtml = require('../../resources/faq');
+const termsHtml = require('../../resources/termsAndConditions');
 
 class HelpController {
   about = async () => {
     return {
       page: 'About',
       htmlContent: aboutHtml,
+    };
+  };
+
+  faq = async () => {
+    return {
+      page: 'FAQ',
+      htmlContent: faqHtml,
+    };
+  };
+
+  privacyPolicy = async () => {
+    return {
+      page: 'Privacy Policy',
+      htmlContent: privacyHtml,
+    };
+  };
+
+  termsAndConditions = async () => {
+    return {
+      page: 'Terms & Conditions',
+      htmlContent: termsHtml,
     };
   };
 }
