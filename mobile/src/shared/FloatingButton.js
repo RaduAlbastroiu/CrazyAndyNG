1e83b4473ec2061f8fb00a5f398effd5b715d134
Radu Albastroiu <albastroiuradug@gmail.com>
2020-09-27 21:11:30 +0300

    Refactor floating button

--- mobile/src/shared/FloatingButton.js
+++ mobile/src/shared/FloatingButton.js
@@ -1,21 +1,21 @@
-import React, {Component, useState} from 'react';
+import React from 'react';
 import {
   View,
-  Text,
   Image,
   StyleSheet,
   TouchableWithoutFeedback,
   Animated,
 } from 'react-native';
-import burger from '../assets/burger.png';
-import more from '../assets/more.png';
-import history from '../assets/history.png';
-import scan from '../assets/scan.png';
-import plus from '../assets/plus.png';
+import burgerImg from '../assets/burger.png';
+import moreImg from '../assets/more.png';
+import historyImg from '../assets/history.png';
+import scanImg from '../assets/scan.png';
+import plusImg from '../assets/plus.png';
+
+const animation = new Animated.Value(0);
 
 const FloatingButton = (props) => {
   let open = false;
-  const animation = new Animated.Value(0);
 
   const toggleMenu = () => {
     const toValue = open ? 0 : 1;
@@ -24,62 +24,6 @@ const FloatingButton = (props) => {
       friction: 6,
     }).start();
     open = !open;
-  };
-
-  const moreStyle = {
-    transform: [
-      {
-        scale: animation,
-      },
-      {
-        translateY: animation.interpolate({
-          inputRange: [0, 1],
-          outputRange: [0, -60],
-        }),
-      },
-    ],
-  };
-
-  const feedbackStyle = {
-    transform: [
-      {
-        scale: animation,
-      },
-      {
-        translateY: animation.interpolate({
-          inputRange: [0, 1],
-          outputRange: [0, -120],
-        }),
-      },
-    ],
-  };
-
-  const historyStyle = {
-    transform: [
-      {
-        scale: animation,
-      },
-      {
-        translateY: animation.interpolate({
-          inputRange: [0, 1],
-          outputRange: [0, -180],
-        }),
-      },
-    ],
-  };
-
-  const scanStyle = {
-    transform: [
-      {
-        scale: animation,
-      },
-      {
-        translateY: animation.interpolate({
-          inputRange: [0, 1],
-          outputRange: [0, -240],
-        }),
-      },
-    ],
   };
 
   const rotation = {
@@ -97,38 +41,54 @@ const FloatingButton = (props) => {
     props.navigation.navigate('More');
   };
 
+  const renderPopButton = (style, img, func) => {
+    return (
+      <TouchableWithoutFeedback onPress={func}>
+        <Animated.View style={style}>
+          <Image source={img} style={styles.subImg} />
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
   // TO DO:
   //  {renderPopButton([styles.button, styles.submenu, scanStyle]), scan}
 
   return (
     <View style={[styles.container, props.style]}>
-      <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.submenu, scanStyle]}>
-          <Image source={scan} style={styles.subImg} />
-        </Animated.View>
-      </TouchableWithoutFeedback>
+      {renderPopButton(
+        [styles.button, styles.submenu, styles.scanStyle],
+        scanImg,
+        () => {
+          console.log('scan');
+        },
+      )}
 
-      <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.submenu, historyStyle]}>
-          <Image source={history} style={styles.subImg} />
-        </Animated.View>
-      </TouchableWithoutFeedback>
+      {renderPopButton(
+        [styles.button, styles.submenu, styles.historyStyle],
+        historyImg,
+        () => {
+          console.log('history');
+        },
+      )}
 
-      <TouchableWithoutFeedback>
-        <Animated.View style={[styles.button, styles.submenu, feedbackStyle]}>
-          <Image source={plus} style={styles.subImg} />
-        </Animated.View>
-      </TouchableWithoutFeedback>
+      {renderPopButton(
+        [styles.button, styles.submenu, styles.feedbackStyle],
+        plusImg,
+        () => {
+          console.log('feedback');
+        },
+      )}
 
-      <TouchableWithoutFeedback onPress={onMorePressed}>
-        <Animated.View style={[styles.button, styles.submenu, moreStyle]}>
-          <Image source={more} style={styles.subImg} />
-        </Animated.View>
-      </TouchableWithoutFeedback>
+      {renderPopButton(
+        [styles.button, styles.submenu, styles.moreStyle],
+        moreImg,
+        onMorePressed,
+      )}
 
       <TouchableWithoutFeedback onPress={toggleMenu}>
-        <Animated.View style={[styles.button, styles.menu, rotation]}>
-          <Image source={burger} style={styles.img} />
+        <Animated.View style={[styles.button, rotation]}>
+          <Image source={burgerImg} style={styles.img} />
         </Animated.View>
       </TouchableWithoutFeedback>
     </View>
@@ -166,17 +126,67 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     shadowColor: '#00213B',
     shadowOpacity: 0.3,
+    backgroundColor: '#00D9C3',
     shadowOffset: {
       height: 10,
     },
-  },
-  menu: {
-    backgroundColor: '#00D9C3',
   },
   submenu: {
     width: 50,
     height: 50,
     borderRadius: 50 / 2,
     backgroundColor: '#FFF',
+  },
+  moreStyle: {
+    transform: [
+      {
+        scale: animation,
+      },
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -60],
+        }),
+      },
+    ],
+  },
+  feedbackStyle: {
+    transform: [
+      {
+        scale: animation,
+      },
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -120],
+        }),
+      },
+    ],
+  },
+  historyStyle: {
+    transform: [
+      {
+        scale: animation,
+      },
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -180],
+        }),
+      },
+    ],
+  },
+  scanStyle: {
+    transform: [
+      {
+        scale: animation,
+      },
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -240],
+        }),
+      },
+    ],
   },
 });
