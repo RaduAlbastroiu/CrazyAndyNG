b229bf76c74f6f344c8ae3c5ea440e53a15d5eef
Radu Albastroiu <albastroiuradug@gmail.com>
2020-09-27 17:31:39 +0300

    Moved server entry point for heroku deployment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // setup environment variables
 require('dotenv').config();
-const connectDB = require('./db');
+const connectDB = require('./backend/db');
 
 // load database
 connectDB();
 
 // spin up server
-const app = require('./app');
+const app = require('./backend/app');
 app.listen(process.env.PORT, () => {
   console.log(`listening on ${process.env.PORT}`);
 });
