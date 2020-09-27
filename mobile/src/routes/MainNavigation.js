4c51783fb5ec6df7c1c703fbd8e40910dbb63316
Radu Albastroiu <albastroiuradug@gmail.com>
2020-09-28 00:10:45 +0300

    Add Html Screen

--- mobile/src/routes/MainNavigation.js
+++ mobile/src/routes/MainNavigation.js
@@ -2,11 +2,7 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Home from '../screens/Home';
 import Tutorial from '../screens/Tutorial';
-import FAQ from '../screens/FAQ';
-import PrivacyPolicy from '../screens/PrivacyPolicy';
-import TermsAndConditions from '../screens/TermsAndConditions';
 import More from '../screens/More';
-import AboutUs from '../screens/AboutUs';
 import HtmlScreen from '../screens/HtmlScreen';
 
 const mainNavigation = {
@@ -26,30 +22,6 @@ const mainNavigation = {
     screen: HtmlScreen,
     navigationOptions: {
       title: 'Help',
-    },
-  },
-  Privacy: {
-    screen: PrivacyPolicy,
-    navigationOptions: {
-      title: 'Privacy Policy',
-    },
-  },
-  FAQ: {
-    screen: FAQ,
-    navigationOptions: {
-      title: 'FAQ',
-    },
-  },
-  About: {
-    screen: AboutUs,
-    navigationOptions: {
-      title: 'About Us',
-    },
-  },
-  Terms: {
-    screen: TermsAndConditions,
-    navigationOptions: {
-      title: 'Terms and Conditions',
     },
   },
 };
--- mobile/src/screens/HtmlScreen.js
+++ mobile/src/screens/HtmlScreen.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import {View, Text, ActivityIndicator} from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  useWindowDimensions,
+} from 'react-native';
 import useHelp from '../hooks/useHelp';
+import HTML from 'react-native-render-html';
 
 export default function HtmlScreen({navigation}) {
   const params = navigation.state.params;
   const {isLoading, data} = useHelp(params.screen);
 
+  const windowDimension = useWindowDimensions().width;
+
   const renderLoading = () => {
     if (isLoading) {
-      return <ActivityIndicator size="large" color="#0000ff" />;
+      return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      );
     }
-    return <Text>{data.page}</Text>;
+    return (
+      <ScrollView style={{flex: 1, paddingHorizontal: 10}}>
+        <HTML html={data.htmlContent} contentWidth={windowDimension} />
+      </ScrollView>
+    );
   };
 
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      {renderLoading()}
-    </View>
-  );
+  return renderLoading();
 }
