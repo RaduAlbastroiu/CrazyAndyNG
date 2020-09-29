8631356ec90e3870a93a24019175eaa791be5eaa
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-09-29 23:53:26 +0300

    Carousel changes

--- mobile/src/screens/Tutorial.js
+++ mobile/src/screens/Tutorial.js
@@ -8,7 +8,7 @@ import {
   SafeAreaView,
   Image,
 } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
 import {useTranslation} from 'react-i18next';
 import TutorialFirst from '../assets/tutorialFirst.png';
 import TutorialSecond from '../assets/tutorialSecond.png';
@@ -18,6 +18,8 @@ import TutorialFourth from '../assets/tutorialFourth.png';
 const arr = [TutorialFirst, TutorialSecond, TutorialThird, TutorialFourth];
 
 const Tutorial = (props) => {
+  // const [entries] = useState();
+  // const [activeSlide, setActiveSlide] = useState();
   const [activeIndex, setActiveIndex] = useState(0);
   const [
     carouselItems = [
@@ -30,10 +32,30 @@ const Tutorial = (props) => {
 
   const {t} = useTranslation();
 
+  const renderSkip = () => {
+    return (
+      <TouchableOpacity onPress={props.onDone} style={styles.buttonBack}>
+        <Text style={styles.appButtonText}>SKIP</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderDone = () => {
+    return (
+      <TouchableOpacity onPress={props.onDone} style={styles.buttonNext}>
+        <Text style={styles.appButtonText}>Done</Text>
+      </TouchableOpacity>
+    );
+  };
+
   const renderItem = ({item, index}) => {
     return (
       <View>
-        <Image source={arr[index]} style={{width: '100%', height: '100%'}} />
+        <View style={styles.buttonsContainer}>
+          {renderSkip()}
+          {renderDone()}
+        </View>
+        <Image source={arr[index]} style={styles.image} />
         <Text style={{fontSize: 30}}>{item.title}</Text>
         <Text>{item.text}</Text>
       </View>
@@ -42,20 +64,42 @@ const Tutorial = (props) => {
   {
     console.log(activeIndex);
   }
+
+  const pagination = () => {
+    return (
+      <Pagination
+        dotsLength={4}
+        activeDotIndex={activeIndex}
+        dotStyle={{
+          position: 'absolute',
+          backgroundColor: 'rgba(255, 255, 255, 0.92)',
+        }}
+        inactiveDotStyle={
+          {
+            // Define styles for inactive dots here
+          }
+        }
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+    );
+  };
   return (
-    <SafeAreaView style={{flex: 1, paddingTop: 50}}>
+    <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
         <Carousel
           layout={'default'}
           ref={(ref) => (carousel = ref)}
           data={carouselItems}
-          sliderWidth={300}
+          sliderWidth={400}
           itemWidth={300}
           renderItem={renderItem}
           dotsLength={4}
           dotColor={'red'}
           onSnapToItem={(index) => setActiveIndex({activeIndex: index})}
+          // onSnapToItem={(index) => setActiveSlide({activeSlide: index})}
         />
+        {pagination()}
       </View>
     </SafeAreaView>
   );
@@ -64,63 +108,10 @@ const Tutorial = (props) => {
 export default Tutorial;
 
 const styles = StyleSheet.create({
-  buttonsContainer: {
-    flexDirection: 'row',
-    marginLeft: 50,
-    position: 'absolute',
-    bottom: -30,
-  },
-  buttonNext: {
-    elevation: 8,
-    backgroundColor: '#009688',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    marginTop: 25,
-    marginLeft: 10,
-    marginRight: 10,
-    width: 150,
-  },
-  buttonBack: {
-    elevation: 8,
-    backgroundColor: '#009688',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    marginTop: 25,
-    width: 150,
-  },
-
   image: {
     alignSelf: 'center',
+    resizeMode: 'cover',
     width: '100%',
     height: '100%',
-    // flexWrap: 'wrap',
-    // flexDirection: 'r ow',
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: '#fff',
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
   },
 });
-
-// renderSkip = () => {
-//   return (
-//     <TouchableOpacity onPress={props.onDone} style={styles.buttonBack}>
-//       <Text style={styles.appButtonText}>SKIP</Text>
-//     </TouchableOpacity>
-//   );
-// };
-
-// renderDone = () => {
-//   return (
-//     <TouchableOpacity onPress={props.onDone} style={styles.buttonNext}>
-//       <Text style={styles.appButtonText}>Done</Text>
-//     </TouchableOpacity>
-//   );
-// };
-
-// return <ImageBackground source={TutorialFirst} style={styles.image} />;
