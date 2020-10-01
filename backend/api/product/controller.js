81c024295da66c93c31c9e4fd94c81bd306afae5
Radu Albastroiu <albastroiuradug@gmail.com>
2020-10-02 01:36:32 +0300

    Start work on product

---
+++ backend/api/product/controller.js
@@ -0,0 +1,38 @@
+class ProductController {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async find() {
+    const foundCategory = await this.model.find();
+    return foundCategory;
+  }
+
+  async create(category) {
+    const duplicate = await this.model.findOne({
+      name: category.name,
+    });
+
+    if (duplicate) throw 'duplicate';
+    const newCategory = new this.model(category);
+    return await newCategory.save();
+  }
+
+  async update(_id, category) {
+    const oldCategory = await this.model.findOne({ _id });
+    if (oldCategory) {
+      oldCategory.name = category.name || oldCategory.name;
+      const newCategory = await oldCategory.save();
+      return newCategory;
+    }
+    throw 'not found';
+  }
+
+  async delete(_id) {
+    const found = await this.model.findOneAndDelete({ _id });
+    if (!found) throw 'not found';
+    return found;
+  }
+}
+
+module.exports = ProductController;
