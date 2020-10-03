fd9b4892261ae2af8c0a6cd0aa6095f7df7ed772
Radu Albastroiu <albastroiuradug@gmail.com>
2020-10-04 00:58:00 +0300

    Finish privacy policy

--- backend/resources/privacyPolicy.js
+++ backend/resources/privacyPolicy.js
@@ -1,20 +1,44 @@
 const htmlContent = `
-<h3>Privacy Policy</h3>
-<p>Styling texts is a very tricky part of converting HTML into react-native components.</p>
-<p>The way react-native's <em>Text</em> components behaves is a lot different from our browsers' implementation.</p>
-<p>Let's see how styles are applied to texts with this plugin.</p>
-<div style="color:red;">This text is inside a div, without a text tag wrapping it. The <em>div</em> tag only has <em>color:red;</em> as style.</div>
-In the example above, you may find, if you inspect the rendered components, that it's the <em>Text</em> component inside that actually receives the color attribute.
-This is because this library parses every text-only style of <em>View</em> wrappers and moves them to each <em>Text</em> child.
-<div style="color:red">
-  <p>This first paragraph doesn't have a specific styling.</p>
-  <p style="color:blue;">This one is blue.</p>
-</div>
-<p>Here, the <em>div</em> wrapper still has <em>color:red;</em> as style.</div>.</p>
-<p>The first paragraph inside it doesn't have any style attribute, either from HTML or from the <em>tagsStyles</em> or <em>classesStyles</em> props.</p>
-<p>The second one is set to be blue from its <em>style</em> attribute.</p>
-<p>You can see the order of priorities that applies to styling. The less important are your <em>tagsStyles</em>, 
-then your <em>classessStyles</em> and finally the styles parsed from your HTML content.</p>
+<h3>隱私政策 Privacy Policy</h3>
+<p>Privacy Policy</p>
+<p>Beanjoy built the ABC app as a Free app. This SERVICE is provided by Beanjoy at no cost and is intended for use as is.</p>
+<p>This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.</p>
+<p>If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.</p>
+<p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at ABC unless otherwise defined in this Privacy Policy.</p>
+<p>Information Collection and Use</p>
+<p>For a better experience, while using our Service, I may require you to provide us with certain personally identifiable information, including but not limited to Y123456(7). The information that I request will be retained on your device and is not collected by me in any way.</p>
+<p>The app does use third party services that may collect information used to identify you.</p>
+<p>Link to privacy policy of third party service providers used by the app</p>
+<ul>
+  <li><a href="https://policies.google.com/privacy">Google Play Services</a></li>
+  <li><a href="https://firebase.google.com/policies/analytics">Google Analytics for Firebase</a></li>
+  <li><a href="https://expo.io/privacy">Expo</a></li>
+</ul>
+<p>Log Data</p>
+<p>I want to inform you that whenever you use my Service, in a case of an error in the app I collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.</p>
+<p>Cookies</p>
+<p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.</p>
+<p>This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+<p>Service Providers</p>
+<p>I may employ third-party companies and individuals due to the following reasons:</p>
+<ul>
+  <li>To facilitate our Service;</li>
+  <li>To provide the Service on our behalf;</li>
+  <li>To perform Service-related services; or</li>
+  <li>To assist us in analyzing how our Service is used</li>
+</ul>
+<p>I want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+<p>Security</p>
+<p>I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.</p>
+<p>Links to Other Sites</p>
+<p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services</p>
+<p>Children’s Privacy</p>
+<p>These Services do not address anyone under the age of 13. I do not knowingly collect personally identifiable information from children under 13. In the case I discover that a child under 13 has provided me with personal information, I immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact me so that I will be able to do necessary actions.</p>
+<p>Changes to This Privacy Policy</p>
+<p>I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page.
+This policy is effective as of 2020-04-29</p>
+<p>Contact Us</p>
+<p>If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at <a href="mailto:crazy2e.biz@gmail.com">crazy2e.biz@gmail.com</a>.</p>
 `;
 
 module.exports = htmlContent;
