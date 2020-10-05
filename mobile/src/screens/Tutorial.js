f5592ffe34c5df4600a142a3de3549c04d46aa04
Radu Albastroiu <albastroiuradug@gmail.com>
2020-10-05 18:01:33 +0300

    Improvements for tutorial

--- mobile/src/screens/Tutorial.js
+++ mobile/src/screens/Tutorial.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -6,10 +6,11 @@ import {
   StyleSheet,
   ImageBackground,
   SafeAreaView,
+  useWindowDimensions,
   Image,
 } from 'react-native';
-import Carousel, { Pagination } from 'react-native-snap-carousel';
-import { useTranslation } from 'react-i18next';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import {useTranslation} from 'react-i18next';
 import TutorialFirst from '../assets/tutorialFirst.png';
 import TutorialSecond from '../assets/tutorialSecond.png';
 import TutorialThird from '../assets/tutorialThird.png';
@@ -17,7 +18,7 @@ import TutorialFourth from '../assets/tutorialFourth.png';
 
 const arr = [TutorialFirst, TutorialSecond, TutorialThird, TutorialFourth];
 
-const Tutorial = ({ navigation }) => {
+const Tutorial = ({navigation}) => {
   let [activeIndex, setActiveIndex] = useState(0);
   const [
     carouselItems = [
@@ -28,7 +29,16 @@ const Tutorial = ({ navigation }) => {
     ],
   ] = useState();
 
-  const { t } = useTranslation();
+  const {t} = useTranslation();
+
+  const windowWidth = useWindowDimensions().width;
+  const dotsWidth = 0.5 * windowWidth;
+  const buttonsWidth = 0.225 * windowWidth;
+  const lateralPadding = 0.025 * windowWidth;
+  console.log(windowWidth);
+  console.log(dotsWidth);
+  console.log(buttonsWidth);
+  console.log(lateralPadding);
 
   const renderSkip = () => {
     return (
@@ -37,8 +47,8 @@ const Tutorial = ({ navigation }) => {
           console.log('Skip pressed');
           navigation.goBack();
         }}
-        style={{ paddingLeft: 12, paddingTop: 12 }}>
-        <Text style={{ color: 'black', fontSize: 18 }}>Skip</Text>
+        style={{paddingLeft: 12, paddingTop: 12}}>
+        <Text style={{color: 'black', fontSize: 18}}>Skip</Text>
       </TouchableOpacity>
     );
   };
@@ -56,12 +66,12 @@ const Tutorial = ({ navigation }) => {
           paddingTop: 12,
           zIndex: 100,
         }}>
-        <Text style={{ color: 'black', fontSize: 18 }}>Done</Text>
+        <Text style={{color: 'black', fontSize: 18}}>Done</Text>
       </TouchableOpacity>
     );
   };
 
-  const renderItem = ({ item, index }) => {
+  const renderItem = ({item, index}) => {
     return (
       <View>
         <ImageBackground source={arr[index]} style={styles.image}>
@@ -70,7 +80,7 @@ const Tutorial = ({ navigation }) => {
               flexDirection: 'row',
             }}></View>
         </ImageBackground>
-        <Text style={{ fontSize: 30 }}>{item.title}</Text>
+        <Text style={{fontSize: 30}}>{item.title}</Text>
         <Text>{item.text}</Text>
       </View>
     );
@@ -111,32 +121,48 @@ const Tutorial = ({ navigation }) => {
         layout={'default'}
         ref={(ref) => (carousel = ref)}
         data={carouselItems}
-        sliderWidth={415}
-        itemWidth={415}
+        sliderWidth={windowWidth}
+        itemWidth={windowWidth}
         renderItem={renderItem}
         onSnapToItem={(index) =>
           setActiveIndex((activeIndex = index))
         }></Carousel>
-      <View style={styles.dotsContainer}>
+      <View
+        style={{
+          position: 'absolute',
+          marginTop: -10,
+          borderColor: 'blue',
+          borderWidth: 1,
+          flexDirection: 'row',
+          width: windowWidth,
+        }}>
         <View
           style={{
-            borderWidth: 2,
+            borderWidth: 1,
             borderColor: 'red',
             height: 50,
-            width: 50,
-          }}></View>
+            width: buttonsWidth,
+            marginLeft: lateralPadding,
+          }}>
+          {renderSkip()}
+        </View>
         <View
           style={{
             height: 50,
-            width: 100,
-          }}></View>
+            width: dotsWidth,
+          }}>
+          {pagination()}
+        </View>
         <View
           style={{
-            borderWidth: 2,
+            borderWidth: 1,
             borderColor: 'red',
             height: 50,
-            width: 50,
-          }}></View>
+            width: buttonsWidth,
+            marginRight: lateralPadding,
+          }}>
+          {renderDone()}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -146,13 +172,12 @@ export default Tutorial;
 const styles = StyleSheet.create({
   dotsContainer: {
     position: 'absolute',
-    width: '50%',
     marginTop: -10,
     borderColor: 'blue',
     borderWidth: 3,
     flexDirection: 'row',
-    alignContent: 'center',
-    alignItems: 'center',
+
+    marginLeft: 50,
     //left: '50%',
     //right: '50%',
   },
