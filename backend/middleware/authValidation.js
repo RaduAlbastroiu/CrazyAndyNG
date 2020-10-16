fce39533ab20dc135a5a54769573844ff7c45ff1
Radu Albastroiu <albastroiuradug@gmail.com>
2020-10-16 18:23:30 +0300

    Fix authentification error

--- backend/middleware/authValidation.js
+++ backend/middleware/authValidation.js
@@ -1,25 +1,28 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
+  let token = req.headers.authorization;
+  token = token.split(' ')[1];
 
-  // Check if no token
-  if (!token) {
+  // Check if admin
+  if (token) {
+    // Verify token
+    try {
+      const decoded = jwt.verify(token, process.env.SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      res.status(401).json({ msg: 'Token is not valid' });
+    }
+  } else {
     // Check if regular user
     if (req.query.deviceId) {
       req.user = { role: 'user', deviceId: req.query.deviceId };
       next();
+    } else {
+      // No token no device id
+      return res.status(401).json({ msg: 'No token, authorization denied' });
     }
-    return res.status(401).json({ msg: 'No token, authorization denied' });
-  }
-
-  // Verify token
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
 
