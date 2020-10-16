6db145e8091aaf5d17f70e0b7cb57f56c3d606a7
Radu Albastroiu <albastroiuradug@gmail.com>
2020-10-16 18:02:25 +0300

    Create validation for admins only

--- backend/middleware/authValidation.js
+++ backend/middleware/authValidation.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-function auth(req, res, next) {
+const auth = (req, res, next) => {
   const token = req.header('x-auth-token');
 
   // Check if no token
@@ -21,6 +21,25 @@ function auth(req, res, next) {
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
-}
+};
 
-module.exports = validate;
+const authAdmin = (req, res, next) => {
+  const token = req.header('x-auth-token');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
+
+module.exports.auth = auth;
+module.exports.authAdmin = authAdmin;
