145481636b3df98951fa601975da62ec75aed09a
Radu Albastroiu <albastroiuradug@gmail.com>
2020-10-18 01:57:37 +0300

    Download blob added in azure utils

--- backend/helpers/azureBlobUtils.js
+++ backend/helpers/azureBlobUtils.js
@@ -4,49 +4,76 @@ let containerClient = null;
 
 const initBlobClient = () => {
   try {
-  const blobServiceClient = BlobServiceClient.fromConnectionString(
-    process.env.AZURE_BLOB
-  );
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      process.env.AZURE_BLOB
+    );
 
-  containerClient = blobServiceClient.getContainerClient(
-    process.env.AZURE_CONTAINER_NAME
-  );
-  } catch(err) {
+    containerClient = blobServiceClient.getContainerClient(
+      process.env.AZURE_CONTAINER_NAME
+    );
+  } catch (err) {
     console.log(err);
   }
 };
 
-const getListOfBlobs = () => {
+const getListOfBlobs = async () => {
   try {
-  let list = await containerClient.listBlobsFlat();
-  return list;
+    let list = await containerClient.listBlobsFlat();
+    return list;
   } catch (err) {
     console.log(err);
     return [];
   }
-}
+};
 
-const uploadBlob = (name, data, dataSize) => {
+const uploadBlob = async (name, data, dataSize) => {
   try {
-  const blockBlobClient = containerClient.getContainerClient(name);
-  await blockBlobClient.upload(data, dataSize);
-  } catch(err) {
+    const blockBlobClient = containerClient.getContainerClient(name);
+    await blockBlobClient.upload(data, dataSize);
+  } catch (err) {
     console.log(err);
     return false;
   }
-}
+};
 
-const deleteBlob = (name) => {
+const downloadBlob = async (name) => {
+  try {
+    blockBlobClient = containerClient.getBlockBlobClient(name);
+
+    const downloadBlockBlobResponse = await blockBlobClient.download(0);
+    return { data: downloadBlockBlobResponse.readableStreamBody };
+  } catch (err) {
+    console.log(err);
+    return {};
+  }
+};
+
+const deleteBlob = async (name) => {
   try {
     const blockBlobClient = containerClient.getContainerClient(name);
     await blockBlobClient.delete();
-    } catch(err) {
-      console.log(err);
-      return false;
-    }
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+// A helper function used to read a Node.js readable stream into a string
+async function streamToString(readableStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    readableStream.on('data', (data) => {
+      chunks.push(data.toString());
+    });
+    readableStream.on('end', () => {
+      resolve(chunks.join(''));
+    });
+    readableStream.on('error', reject);
+  });
 }
 
 module.exports.initBlobClient = initBlobClient;
 module.exports.getListOfBlobs = getListOfBlobs;
 module.exports.uploadBlob = uploadBlob;
+module.exports.downloadBlob = downloadBlob;
 module.exports.deleteBlob = deleteBlob;
