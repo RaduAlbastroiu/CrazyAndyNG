d94b322ab522107549b8742aeb931c0fab834324
Radu Albastroiu <albastroiuradug@gmail.com>
2020-10-27 02:41:43 +0200

    Improved product catalog

--- mobile/src/screens/ProductCatalog.js
+++ mobile/src/screens/ProductCatalog.js
@@ -7,8 +7,10 @@ import {
   useWindowDimensions,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
+import {ScrollView} from 'react-native-gesture-handler';
+
 import TopSearch from '../components/TopSearch';
-import Placeholder from '../assets/placeholder.png';
+import SmallProduct from '../components/SmallProduct';
 
 // here just to see the data
 const productsMockup = [
@@ -104,19 +106,176 @@ const productsMockup = [
     size: '44',
     category: 'Tvs',
   },
+  {
+    price: [1399.99, 1999.99],
+    hashtags: [
+      'cool',
+      'tv',
+      '4k',
+      'something',
+      'co2ol',
+      'tv',
+      '43k',
+      'somethin4g',
+    ],
+    images: [
+      'image7d2cbf00-8ed5-45c7-bab0-d87f702b9cf7.jpg',
+      'imagea16c6807-f7a8-446a-9821-8cc92c2f12d9.jpg',
+    ],
+    name: 'Televizor Sony, 108 cm',
+    brand: 'LG',
+    barcode: 'barcode234',
+    origin: 'US',
+    size: '44',
+    category: 'Tvs',
+  },
+  {
+    price: [1999.99],
+    hashtags: [
+      'cool',
+      'tv',
+      '4k',
+      'something',
+      'co2ol',
+      'tv',
+      '43k',
+      'somethin4g',
+    ],
+    images: [
+      'image7d2cbf00-8ed5-45c7-bab0-d87f702b9cf7.jpg',
+      'imagea16c6807-f7a8-446a-9821-8cc92c2f12d9.jpg',
+    ],
+    name: 'Televizor Sony, 108 cm',
+    brand: 'LG',
+    barcode: 'barcode234',
+    origin: 'US',
+    size: '44',
+    category: 'Tvs',
+  },
+  {
+    price: [1799.99, 1999.99],
+    hashtags: [
+      'cool',
+      'tv',
+      '4k',
+      'something',
+      'co2ol',
+      'tv',
+      '43k',
+      'somethin4g',
+    ],
+    images: [
+      'image7d2cbf00-8ed5-45c7-bab0-d87f702b9cf7.jpg',
+      'imagea16c6807-f7a8-446a-9821-8cc92c2f12d9.jpg',
+    ],
+    name: 'Televizor Sony, 108 cm',
+    brand: 'LG',
+    barcode: 'barcode234',
+    origin: 'US',
+    size: '44',
+    category: 'Tvs',
+  },
+  {
+    price: [1999.99],
+    hashtags: [
+      'cool',
+      'tv',
+      '4k',
+      'something',
+      'co2ol',
+      'tv',
+      '43k',
+      'somethin4g',
+    ],
+    images: [
+      'image7d2cbf00-8ed5-45c7-bab0-d87f702b9cf7.jpg',
+      'imagea16c6807-f7a8-446a-9821-8cc92c2f12d9.jpg',
+    ],
+    name: 'Televizor Sony, 108 cm',
+    brand: 'LG',
+    barcode: 'barcode234',
+    origin: 'US',
+    size: '44',
+    category: 'Tvs',
+  },
+  {
+    price: [1799.99, 1999.99],
+    hashtags: [
+      'cool',
+      'tv',
+      '4k',
+      'something',
+      'co2ol',
+      'tv',
+      '43k',
+      'somethin4g',
+    ],
+    images: [
+      'image7d2cbf00-8ed5-45c7-bab0-d87f702b9cf7.jpg',
+      'imagea16c6807-f7a8-446a-9821-8cc92c2f12d9.jpg',
+    ],
+    name: 'Televizor Sony, 108 cm',
+    brand: 'LG',
+    barcode: 'barcode234',
+    origin: 'US',
+    size: '44',
+    category: 'Tvs',
+  },
+  {
+    price: [1799.99, 1999.99],
+    hashtags: [
+      'cool',
+      'tv',
+      '4k',
+      'something',
+      'co2ol',
+      'tv',
+      '43k',
+      'somethin4g',
+    ],
+    images: [
+      'image7d2cbf00-8ed5-45c7-bab0-d87f702b9cf7.jpg',
+      'imagea16c6807-f7a8-446a-9821-8cc92c2f12d9.jpg',
+    ],
+    name: 'Televizor Sony, 108 cm',
+    brand: 'LG',
+    barcode: 'barcode234',
+    origin: 'US',
+    size: '44',
+    category: 'Tvs',
+  },
 ];
 
 const ProductCatalog = ({route, navigation}) => {
+  const windowWidth = useWindowDimensions().width;
+
+  renderProducts = () => {
+    return productsMockup.map((product) => {
+      return (
+        <SmallProduct
+          size={{
+            width: (windowWidth / 7) * 3,
+          }}
+          product={product}
+        />
+      );
+    });
+  };
+
   return (
-    <View>
-      <TopSearch />
-      <View
-        style={{
-          borderColor: 'red',
-          borderWidth: 2,
+    <View style={{backgroundColor: 'white'}}>
+      <TopSearch category="Masks" />
+      <ScrollView
+        contentContainerStyle={{
+          zIndex: 6000,
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          paddingBottom: 80,
+          justifyContent: 'center',
         }}>
-        <Text>ProductCatalog</Text>
-      </View>
+        {renderProducts()}
+      </ScrollView>
     </View>
   );
 };
