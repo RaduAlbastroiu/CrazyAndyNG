7ff632e68eff189350a24e6cbe209880707f5454
Radu Albastroiu <albastroiuradug@gmail.com>
2020-11-16 05:09:01 +0200

    Improve navigation redux

--- mobile/src/redux/actions/navigationActions.js
+++ mobile/src/redux/actions/navigationActions.js
@@ -1,10 +1,38 @@
 import {SHOW_BARCODE} from '../types';
+import {SHOW_PRODUCT_LOADING} from '../types';
+import {SHOW_PRODUCT_NOT_FOUND} from '../types';
 
 export const setShowBarcode = (showBarcode) => async (dispatch) => {
   try {
     dispatch({
       type: SHOW_BARCODE,
       payload: showBarcode,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const setShowProductLoading = (showProductLoading) => async (
+  dispatch,
+) => {
+  try {
+    dispatch({
+      type: SHOW_PRODUCT_LOADING,
+      payload: showProductLoading,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const setShowProductNotFound = (showProductNotFound) => async (
+  dispatch,
+) => {
+  try {
+    dispatch({
+      type: SHOW_PRODUCT_NOT_FOUND,
+      payload: showProductNotFound,
     });
   } catch (err) {
     console.log(err);
--- mobile/src/redux/reducers/navigationReducer.js
+++ mobile/src/redux/reducers/navigationReducer.js
@@ -1,14 +1,29 @@
 import {SHOW_BARCODE} from '../types';
+import {SHOW_PRODUCT_LOADING} from '../types';
+import {SHOW_PRODUCT_NOT_FOUND} from '../types';
 
 const initialState = {
   showBarcode: true,
+  showProductLoading: false,
+  showProductNotFound: false,
 };
 
 const navigationReducer = (state = initialState, action) => {
   switch (action.type) {
     case SHOW_BARCODE:
       return {
+        ...state,
         showBarcode: action.payload,
+      };
+    case SHOW_PRODUCT_LOADING:
+      return {
+        ...state,
+        showProductLoading: action.payload,
+      };
+    case SHOW_PRODUCT_NOT_FOUND:
+      return {
+        ...state,
+        showProductNotFound: action.payload,
       };
     default:
       return state;
